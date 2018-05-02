76d1593cb179a43386b42dffd4064fb172196e80
Luis Roberto Borba Ferreira <luis.ferreira@SLT-002066.solutis.net.br>
2018-05-02 18:20:00 -0300

    Adicionando projeto ao repositório

---
+++ eventoapp/src/main/java/br/com/eventoapp/eventoapp/model/Evento.java
@@ -0,0 +1,41 @@
+package br.com.eventoapp.eventoapp.model;
+
+public class Evento {
+
+    private String nome;
+    private String local;
+    private String data;
+    private String horario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+}
