28a2fc28ea9d72d010f12031656061d7621750c1
Luis Roberto Borba Ferreira <luis.ferreira@SLT-002066.solutis.net.br>
2018-05-04 10:49:39 -0300

    Configurando Conexão ao Banco

--- eventoapp/src/main/java/br/com/eventoapp/eventoapp/DataConfiguration.java
+++ eventoapp/src/main/java/br/com/eventoapp/eventoapp/DataConfiguration.java
@@ -1,8 +1,37 @@
 package br.com.eventoapp.eventoapp;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DataConfiguration {
-    
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/eventoapp");
+        dataSource.setUsername("root");
+        dataSource.setPassword("luisrbferreira");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+        adapter.setPrepareConnection(true);
+
+        return adapter;
+    }
 }
--- eventoapp/src/main/java/br/com/eventoapp/eventoapp/model/Evento.java
+++ eventoapp/src/main/java/br/com/eventoapp/eventoapp/model/Evento.java
@@ -1,11 +1,32 @@
 package br.com.eventoapp.eventoapp.model;
 
-public class Evento {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Evento implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long codigo;
 
     private String nome;
     private String local;
     private String data;
     private String horario;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
 
     public String getNome() {
         return nome;
